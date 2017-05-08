8c137f36a17a01f246d37fd1ccc9d64d1004f45d
JonCass <JCass1980@gmail.com>
2017-05-08 15:03:47 -0700

    match found need to move into state to use i think

--- src/components/UserMatches.js
+++ src/components/UserMatches.js
@@ -36,22 +36,91 @@ const styles={
     }
 }
 class UserMatches extends React.Component {
-//   constructor(props) {
- 
-//    super(props)
-//   }
+   constructor(props) {
+        super(props)
+        this.state = {
+            matchIDs : []
+        }
+   }
 
-  createCurrentUserArray = () => {
+  handleMatches = () => {
       
       if(this.props.availabilityArray){
-      var currentUserArray = this.props.availabilityArray.filter(function(entry){
-        return( entry.user_id === this.props.currentUserID)
-      }.bind(this))
-      console.log('currentUserArray in UserMatches.js', currentUserArray)
+        var currentUserArray = this.props.availabilityArray.filter(function(entry){
+            return( entry.user_id === this.props.currentUserID)
+        }.bind(this))
+        // console.log('currentUserArray in UserMatches.js', currentUserArray)
+        // console.log('this.props.availabilityArray', this.props.availabilityArray )
       }
+      if(this.props.availabilityArray){
+        var fromTime = 0
+        var toTime = 0
+        var cUserFrom = 0
+        var cUserTo = 0
+        for( let i = 0; i < this.props.availabilityArray.length ; i++ ){
+            for( let j = 0; j < currentUserArray.length ; j++ ){
+                if(currentUserArray[j].quadrant === this.props.availabilityArray[i].quadrant && currentUserArray[j].day_of_week === this.props.availabilityArray[i].day_of_week && currentUserArray[j].user_id !== this.props.availabilityArray[i].user_id){
+                    if(this.props.availabilityArray[i].from_num === 12 && this.props.availabilityArray[i].from_suffix === 'pm'){
+                        fromTime = 12
+                    }
+                    else if(this.props.availabilityArray[i].from_num === 12 && this.props.availabilityArray[i].from_suffix === 'am'){
+                        fromTime = 24
+                    }
+                    else if(this.props.availabilityArray[i].from_suffix === 'am'){
+                        fromTime = this.props.availabilityArray[i].from_num
+                    }
+                    else if(this.props.availabilityArray[i].from_suffix === 'pm'){
+                        fromTime = this.props.availabilityArray[i].from_num + 12
+                    }
+                    console.log('UserMatches adjusted fromTime', fromTime)
+                    if(this.props.availabilityArray[i].to_num === 12 && this.props.availabilityArray[i].to_suffix === 'pm'){
+                        toTime = 12
+                    }
+                    else if(this.props.availabilityArray[i].to_num === 12 && this.props.availabilityArray[i].to_suffix === 'am'){
+                        toTime = 24
+                    }
+                    else if(this.props.availabilityArray[i].to_suffix === 'am'){
+                        toTime = this.props.availabilityArray[i].to_num
+                    }
+                    else if(this.props.availabilityArray[i].to_suffix === 'pm'){
+                        toTime = this.props.availabilityArray[i].to_num + 12
+                    }
+                    console.log('UserMatches adjusted toTime', toTime)
+                    if(currentUserArray[j].from_num === 12 && currentUserArray[j].from_suffix === 'pm'){
+                        cUserFrom = 12
+                    }
+                    else if(currentUserArray[j].from_num === 12 && currentUserArray[j].from_suffix === 'am'){
+                        cUserFrom = 24
+                    }
+                    else if(currentUserArray[j].from_suffix === 'am'){
+                        cUserFrom = currentUserArray[j].from_num
+                    }
+                    else if(currentUserArray[j].from_suffix === 'pm'){
+                        cUserFrom = currentUserArray[j].from_num + 12
+                    }
+                    console.log('Current Users Time from adjusted ', cUserFrom)
+                    if(currentUserArray[j].to_num === 12 && currentUserArray[j].to_suffix === 'pm'){
+                        cUserTo = 12
+                    }
+                    else if(currentUserArray[j].to_num === 12 && currentUserArray[j].to_suffix === 'am'){
+                        cUserTo = 24
+                    }
+                    else if(currentUserArray[j].to_suffix === 'am'){
+                        cUserTo = currentUserArray[j].to_num
+                    }
+                    else if(currentUserArray[j].to_suffix === 'pm'){
+                        cUserTo = currentUserArray[j].to_num + 12
+                    }
+                    console.log('Current Users Time To Adjusted', cUserTo)
+                    if((cUserFrom < toTime) && (cUserTo > fromTime)){
+                        console.log("Match found with current user and user id" , this.props.availabilityArray[i].user_id )
+                    }
+                }
+            }
+      }}
   }
   render() {
-      this.createCurrentUserArray()
+      this.handleMatches()
     return (
       <div style={styles.matchesContainer}>
         <div style={styles.matchBox}>
