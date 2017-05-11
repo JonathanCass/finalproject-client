698c24851a5dbff4ffdc58629cf6d81923012a53
Josephine Gabrielle Baltazar <jgsbaltazar@yahoo.com>
2017-05-11 10:31:05 -0700

    failed attempt. mustsave checkpoint

--- src/components/NewComponent.js
+++ src/components/NewComponent.js
@@ -2,100 +2,16 @@ import React from 'react'
 import styles from './ParkView.styles'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import DatePicker from 'material-ui/DatePicker'
-import Table from './ParkViewTable'
-// import {postCreateActivity} from '../api/messaging'
-import '../assets/home.css'
-import { connect } from 'react-redux'
-import { getParks } from '../api/messaging'
 
-class ParkView extends React.Component {
-  constructor() {
-   super()
-   this.state = {
-        id:'',
-        play:'',
-        level:'',
-        activities:'',
-        // date:'',
-        notes:'',
-        start:'',
-        gear:'',
-        park:'',
-        activityArray:[]
-   }
-}
-createActivity = (e) => {
-  // console.log(createActivityObj)
-  e.preventDefault()
-  var createActivityObj ={
-      play:this.state.play,
-      level:this.state.level,
-      activities:this.state.activities,
-      // date:this.state.date,
-      notes:this.state.notes,
-      start:this.state.start,
-      gear:this.state.gear,
-      park:this.state.park
-  }
-  // postCreateActivity(createActivityObj)
-  this.setState({
-    activityArray : [...this.state.activityArray, createActivityObj],
-    play:'', level:'', activities:'', notes:'', start:'', daynight:'', gear:'', park:''
-  })
-}
-handleChange = (e) => {
-  this.setState({
-    [e.target.name]:e.target.value
-  })    
-}
-handleButton = (e) => {
-  this.setState({
-    [e.target.name]:e.target.value   
-  })
-  console.log(e.target.value)
+class New extends React.Component {
+    constructor() {
+        super()
+    }
 
-}
-handleLevel = (e) => { // for Type of Play/Experience
-  this.setState({
-    level: e.target.value,
-  })
-}
-
-componentWillMount() {
-  getParks()
-}
-
-  render() {
-    return (
-      <div style={styles.container}>     
-          <h2 style={styles.h2}>Type Of Play</h2>
-          <div style={styles.radioPlay}>
-            <input type='radio' onChange={this.handleButton} name='play' value="Competitive"/>
-            <label htmlFor="competitive" style={styles.radio}>Competitive</label>
-            <input type='radio'onChange={this.handleButton} name='play' value="Leisurely"/>
-            <label htmlFor="leisurely" style={styles.radio}>Leisurely</label>
-          <div className='line' style={styles.line}></div>
-          </div>
-          <div className='level'style={styles.level}>
-            <input type='checkbox' onChange={this.handleLevel} name='level' value="Beginner"/>
-            <label htmlFor='beginner' style={styles.levelBoxes}>Beginner</label>
-            <input type='checkbox' onChange={this.handleLevel} name='level' value="Experienced"/>
-            <label htmlFor='experienced' style={styles.levelBoxes}>Experienced</label>
-            <input type='checkbox'onChange={this.handleLevel} name='level' value="Advanced"/>
-            <label htmlFor='advanced' style={styles.levelBoxes}>Advanced</label>
-          </div>
-         <div>
-            <input type='radio' style={styles.radioQuad} id="northwestloc" name='quadrant' value="Northwest"/>
-            <label htmlFor='northwest' style={styles.radioLabel}>Northwest</label>
-            <input type='radio' style={styles.radioQuad} id="southwestloc" name='quadrant' value="Southwest"/>
-            <label htmlFor='southwest' style={styles.radioLabel}>Southwest</label>
-            <input type='radio' style={styles.radioQuad} id="northeastloc" name='quadrant' value="Northeast"/>
-            <label htmlFor='northeast' style={styles.radioLabel}>Northeast</label>
-            <input type='radio' style={styles.radioQuad} id="southeastloc" name="quadrant" value="Southeast"/>
-            <label htmlFor='southeast' style={styles.radioLabel}>Southeast</label>
-          
-           
-           <div id='northwest'> 
+        render() {
+        return (
+            <div>
+                <div id='northwest'> 
              <select onChange={this.handleButton} name="park" value={this.state.parks} style={styles.parksBox}>
                <option>North West Parks</option>  
                {this.props.parks.filter((park, i)=>{
@@ -110,8 +26,8 @@ componentWillMount() {
                   return park.quadrant === 'northwest'
                }).map(park =>{  
                   if(park.basketball === 'yes'){
-                  console.log(park.name, 'has basketball') }
-                {/*} if(park.volleyball === 'yes'){
+                  console.log(park.name, 'has basketball') 
+                } if(park.volleyball === 'yes'){
                   console.log(park.name, 'has volleyball')
                 } if(park.walking === 'yes') {
                   console.log(park.name, 'has walking')
@@ -121,7 +37,7 @@ componentWillMount() {
                   console.log(park.name, 'has tennis')
                 } if(park.frisbee_gold === 'yes'){
                   console.log(park.name, 'has frisbee golf')
-                }*/}
+                }
                })}            
              </select>
              <MuiThemeProvider>
@@ -304,20 +220,10 @@ componentWillMount() {
                 <option value="PM">PM</option>
               </select>
              </div>
-             <div className='notes'>
-                <textarea onChange={this.handleChange} name='notes' value={this.state.notes} style={styles.notes} placeholder="Additional Notes"></textarea>
-             </div>
-             <button onClick={this.createActivity} style={styles.create}>Create</button><h5 style={styles.or}>Or</h5>
-             <button onClick={this.handleBrowse} style={styles.browse}>Browse</button>
            </div>
-          </div>       
-          <Table activityArray={[...this.state.activityArray]}/>
-      </div> // end of container
-    )
-  }
+        </div> //container
+        )
+    }          
 }
 
-function mapStateToProps(appState) {
-  return { dbUsers: appState.dbUsers, currentUserID : appState.currentUserId, parks: appState.parks}
-}
-export default connect(mapStateToProps)(ParkView)
+export default New
